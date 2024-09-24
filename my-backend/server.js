87650f3bd72b40de292b4605d3e0aac5142ae68c
HamzaAsad <HamzaAsad>
2024-09-24 17:13:17 +0500

    Removed submodule and added backend as a regular folder

---
+++ my-backend/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const router = require('./routes/router.js');
+require('dotenv').config();
+
+// Enable CORS for requests from your frontend (http://localhost:3000)
+const corsOrigin = process.env.NODE_ENV === 'production' ? 'https://your-frontend-url.vercel.app' : 'http://localhost:3000';
+
+app.use(cors({
+  origin: corsOrigin
+}));
+
+
+// Middleware to parse incoming JSON requests
+app.use(express.json());
+
+// Use your custom router
+app.use('/', router); // Ensure you’re using the correct path for your router
+
+// Root route for basic checks
+app.get('/', (req, res) => {
+    res.send('Server is running');
+});
+
+// Start server on port 5000
+app.listen(5000, () => {
+    console.log('Server is running on port 5000');
+});
